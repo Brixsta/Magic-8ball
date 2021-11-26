053e97aa1467a74bb43f7b1d424c154050ad7209
Charles <Brixsta@gmail.com>
2021-11-26 09:56:03 -0600

    shortened audio

--- scripts.js
+++ scripts.js
@@ -59,7 +59,7 @@ const generateRandomWisdom = () => {
         "It is certain.", "It is decidedly so.", "Without a doubt.",
         "Yes defintely.", "You may rely on it.", "As I see it, yes.",
         "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.",
-        "Reply hazy, try again.", "Ask again later.", "Better not tell you now.",
+        "Reply hazy, try again.", "Ask again later.", "Won't tell you now.",
         "Cannot predict now.", "Don't count on it.", "My reply is no.",
         "My sources say no.", "Outlook not so good.", "Very doubtful."
     ];
